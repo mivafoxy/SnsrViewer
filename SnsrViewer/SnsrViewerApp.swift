bbfb8557f8075b942881db2327866b764918640c
malakhoviv <malakhoviv@osbank.ru>
2023-05-04 16:15:53 +0300

    Initial Commit

---
+++ SnsrViewer/SnsrViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnsrViewerApp.swift
+//  SnsrViewer
+//
+//  Created by Илья Малахов on 04.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SnsrViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
